d1926f8f01de82d57faa560b55b122ba2402d631
Yassine El Anssari <yassinedvp25@gmail.com>
2026-04-01 12:28:30 +0100

    feat(core): add loading system, API endpoints, and assets management

---
+++ lib/core/network/api_endpoints.dart
@@ -0,0 +1,9 @@
+class ApiEndPoints {
+  static const String baseUrl = 'https://dummyjson.com';
+  static const String login = '$baseUrl/auth/login';
+  static const String register = '$baseUrl/users/add';
+  static const String fetchProducts = '$baseUrl/products';
+  static const String categories = '$baseUrl/products/categories';
+  static const String categProduct = '$baseUrl/products/category';
+  static const String carts = '$baseUrl/carts';
+}
